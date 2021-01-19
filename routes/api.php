1a9d1b4a7c0c29599759a2a27e6b1cdcb850085f
bhowmik23 <biddut35-2140@diu.edu.bd>
2021-01-19 16:59:35 +0600

    laravel JWT implement for auth

--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,18 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::apiResource('/class', ClassesController::class);
 Route::apiResource('/subject', SubjectController::class);
+
+Route::group([
+
+    'middleware' => 'api',
+    'namespace' => 'App\Http\Controllers',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
