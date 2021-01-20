abbf878de7141f774fe6fe4ba08a9d0170699e5d
bhowmik23 <biddut35-2140@diu.edu.bd>
2021-01-20 12:12:15 +0600

    pauload and claims

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -29,7 +29,7 @@ class AuthController extends Controller
     {
         $credentials = $request->only('email', 'password');
 
-        if ($token = $this->guard()->attempt($credentials)) {
+        if ($token = $this->guard()->claims(['name' => 'Biddut bhowmik'])->attempt($credentials)) {
             return $this->respondWithToken($token);
         }
 
@@ -92,5 +92,11 @@ class AuthController extends Controller
     public function guard()
     {
         return Auth::guard();
+    }
+
+    public function payload()
+    {
+        // $payload = auth()->payload();
+        return auth()->payload();
     }
 }
